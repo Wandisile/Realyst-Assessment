0f40f296f3b8bdc2998ae14ab9a826dece2fc295
wandisilen@dotcomsoftware.co.za <wandisilen@dotcomsoftware.co.za>
2022-11-07 14:34:51 +0200

    generate random 5x5 grid

---
+++ Web/WordSearcher/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Random random = new Random();
+        String characters = "abcdefghijklmnopqrstuvwxyz";
+
+        var matrix = new int[5, 5];
+
+        for (int i = 0; i < 5; i++)
+        {
+            char randomLetter;
+            for (int j = 0; j < 5; j++)
+            {
+                matrix[i, j] = random.Next(characters.Length);
+                randomLetter = characters[matrix[i, j]];
+                Console.Write(randomLetter + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
