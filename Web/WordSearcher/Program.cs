cf2dca18dfe007d37edc2c415b21a1177f08fba8
wandisilen@dotcomsoftware.co.za <wandisilen@dotcomsoftware.co.za>
2022-11-09 10:41:08 +0200

    wordsearch

--- Web/WordSearcher/Program.cs
+++ Web/WordSearcher/Program.cs
@@ -1,22 +1,41 @@
-﻿internal class Program
+﻿using WordSearcher;
+
+internal class Program
 {
+    private static void search(int[,] mat, int n, int x)
+    {
+        // set indexes for top right
+        // element
+        int i = 0, j = n - 1;
+
+        while (i < n && j >= 0)
+        {
+            if (mat[i, j] == x)
+            {
+                Console.WriteLine(mat[i, j]);
+                Console.Write("Element found at " + i + ", "
+                              + j);
+                return;
+            }
+
+            if (mat[i, j] > x)
+                j--;
+            else // if mat[i][j] < x
+                i++;
+        }
+
+        Console.Write("n Element not found");
+        return; // if ( i==n || j== -1 )
+    }
     private static void Main(string[] args)
     {
-        Random random = new Random();
-        String characters = "abcdefghijklmnopqrstuvwxyz";
+        int[,] mat = { { 10, 20, 30, 40 },
+                        { 15, 25, 35, 45 },
+                        { 27, 29, 37, 48 },
+                        { 32, 33, 39, 50 } };
 
-        var matrix = new int[5, 5];
-
-        for (int i = 0; i < 5; i++)
-        {
-            char randomLetter;
-            for (int j = 0; j < 5; j++)
-            {
-                matrix[i, j] = random.Next(characters.Length);
-                randomLetter = characters[matrix[i, j]];
-                Console.Write(randomLetter + "\t");
-            }
-            Console.WriteLine();
-        }
+        // Function call
+        search(mat, 4, 29);
     }
+
 }
